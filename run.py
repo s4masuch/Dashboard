f6fb43b1c282cd13ad0e2a42c2c5bb9fca481da9
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-13 17:32:20 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
+# Do not use app.run_server here
+# Instead, simply import the 'app' object from your 'main' module
 from main import app
-
-if __name__ == '__main__':
-    app.run_server(port=8060)
