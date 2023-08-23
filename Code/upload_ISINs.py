bd9a632a0d5acf5be88e287befb32469f61ba436
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-23 09:05:23 +0200

    Update upload_ISINs.py

--- Code/upload_ISINs.py
+++ Code/upload_ISINs.py
@@ -1,18 +1,21 @@
-# upload_utils.py
+import os
 import pandas as pd
-import yfinance as yf  # Replace with the appropriate library for fetching data
+import yfinance as yf
 
-def check_isin(isin):
-    try:
-        _ = yf.Ticker(isin)
-        return True
-    except:
-        return False
+# ... (other functions)
 
-def upload_isins(isin_list):
-    processed = 0
+def upload_isins_from_file(file_path):
+    # Read ISINs from the provided file_path
+    isin_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            isin_list.append(line.strip())
+
+    processed_count = 0
     for isin in isin_list:
         if check_isin(isin):
-            processed += 1
-    return processed
+            processed_count += 1
+    return processed_count
 
+# This function now takes the file_path as an argument
+# Call this function in your main script with the appropriate file path
