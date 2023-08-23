dc98a6269aad25747d9140228b6eb692eb515700
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-23 09:02:13 +0200

    Update upload_ISINs.py

--- Code/upload_ISINs.py
+++ Code/upload_ISINs.py
@@ -1,1 +1,18 @@
+# upload_utils.py
+import pandas as pd
+import yfinance as yf  # Replace with the appropriate library for fetching data
+
+def check_isin(isin):
+    try:
+        _ = yf.Ticker(isin)
+        return True
+    except:
+        return False
+
+def upload_isins(isin_list):
+    processed = 0
+    for isin in isin_list:
+        if check_isin(isin):
+            processed += 1
+    return processed
 
