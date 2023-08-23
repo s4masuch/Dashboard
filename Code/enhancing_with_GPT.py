a7fd6cbcf6566b362217c0a267f6b71d2fdd2bdf
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-23 11:18:44 +0200

    Update enhancing_with_GPT.py

--- Code/enhancing_with_GPT.py
+++ Code/enhancing_with_GPT.py
@@ -1,17 +1,16 @@
 import openai
 import pandas as pd
 
-
 def enhance_data(api_key, excel_filename):
+    openai.api_key = api_key
+    
     try:
-        openai.api_key = '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'
         openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[])
     except openai.error.AuthenticationError as e:
-        return f"Error: Incorrect API key provided - {e.message}"
+        return f"Error: Incorrect API key provided - {str(e)}"
     
     excel_filename = 'Data/Data_Frames/latest_financials.xlsx'
     
-    openai.api_key = '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'
     # Load the latest financials data from Excel
     financials_df = pd.read_excel(excel_filename, sheet_name='FinancialsShort')
     
@@ -43,7 +42,7 @@ def enhance_data(api_key, excel_filename):
     return reply
 
 # Example usage
-api_key = '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'
+api_key = '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'
 excel_filename = 'Data/Data_Frames/latest_financials.xlsx'
 enhanced_reply = enhance_data(api_key, excel_filename)
 print(f"ChatGPT: {enhanced_reply}")
