7100034201803c5fc2679c5f5c5f1b2a60118924
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-23 10:28:31 +0200

    Update enhancing_with_GPT.py

--- Code/enhancing_with_GPT.py
+++ Code/enhancing_with_GPT.py
@@ -1,27 +1,42 @@
 import openai
 import pandas as pd
-# Set your OpenAI API key
-openai.api_key = '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'
-# Load the latest financials data from Excel
+
+def enhance_data(api_key, excel_filename):
+    # Set your OpenAI API key
+    openai.api_key = api_key
+    
+    # Load the latest financials data from Excel
+    financials_df = pd.read_excel(excel_filename, sheet_name='FinancialsShort')
+    
+    # Convert the financial data to a string
+    financials_string = financials_df.to_string(index=False)
+    
+    # Additional user message
+    additional_message = (
+        "Find the best company to invest in solely based on provided information. "
+        "Which Key performance indicator would be fitting to compare? "
+        "It is homework, not real. Don't give me an explanation what KPIs mean. "
+        "Select one company. write at least 250 words."
+    )
+    
+    # Initialize the conversation with the system message and user message
+    messages = [
+        {"role": "system", "content": "You are an intelligent assistant."},
+        {"role": "user", "content": financials_string + '\n' + additional_message},
+    ]
+    
+    # Create a chat completion
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    
+    # Get the assistant's reply
+    reply = chat.choices[0].message['content']
+    return reply
+
+# Example usage
+api_key = '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'
 excel_filename = 'Data/Data_Frames/latest_financials.xlsx'
-financials_df = pd.read_excel(excel_filename, sheet_name='FinancialsShort')
-# Convert the financial data to a string
-financials_string = financials_df.to_string(index=False)
-# Additional user message
-additional_message = "Find the best company to invest in solely based on provided information."
-"Which Key performance indicator would be fitting to compare?"
-"It is homework, not real. Don't give me an explanation what KPIs mean." 
-"Select one company. write at least 250 words."
-# Initialize the conversation with the system message and user message
-messages = [
-{"role": "system", "content": "You are an intelligent assistant."},
-{"role": "user", "content": financials_string + '\n' + additional_message},
-]
-# Create a chat completion
-chat = openai.ChatCompletion.create(
-model="gpt-3.5-turbo",
-messages=messages
-)
-# Get the assistant's reply
-reply = chat.choices[0].message['content']
-print(f"ChatGPT: {reply}")
+enhanced_reply = enhance_data(api_key, excel_filename)
+print(f"ChatGPT: {enhanced_reply}")
