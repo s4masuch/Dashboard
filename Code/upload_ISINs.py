4616db034d0ace81830855bb018d0b5589e8bf1e
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-26 19:08:00 +0200

    Update upload_ISINs.py

--- Code/upload_ISINs.py
+++ Code/upload_ISINs.py
@@ -3,14 +3,11 @@ import requests
 import base64
 
 GITHUB_API_BASE_URL = "https://api.github.com"
-REPO_OWNER = "s4masuch"
-REPO_NAME = "Dashboard"
-FILE_PATH = "Code/Data/ISIN-Upload/ISIN-Input.csv"  # URL-encoded path
 
 def upload_isins_to_github(file_path, file_content):
     github_token = os.getenv("GITHUB_TOKEN")
-    repo_owner = app.config["REPO_OWNER"]  # Retrieve from app.config
-    repo_name = app.config["REPO_NAME"]  # Retrieve from app.config
+    repo_owner = os.getenv("REPO_OWNER")
+    repo_name = os.getenv("REPO_NAME")
     encoded_file_path = requests.utils.quote(file_path)  # URL-encode the file path
 
     if not github_token:
@@ -22,12 +19,19 @@ def upload_isins_to_github(file_path, file_content):
         "Authorization": f"Bearer {github_token}"
     }
 
-    response = requests.put(url, json={
-        "message": "Update ISIN file",
-        "content": file_content
-    }, headers=headers)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return "File updated on GitHub successfully"
+        file_sha = response.json()["sha"]
+        response = requests.put(url, json={
+            "message": "Update ISIN file",
+            "content": file_content,
+            "sha": file_sha
+        }, headers=headers)
+
+        if response.status_code == 200:
+            return "File updated on GitHub successfully"
+        else:
+            return f"Error updating file on GitHub: {response.text}"
     else:
-        return f"Error updating file on GitHub: {response.text}"
+        return f"Error finding file on GitHub: {response.text}"
