8ed9869bf348c8127829ecb2b56b45e03d8126f0
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-08-26 14:21:50 +0200

    Update upload_ISINs.py

--- Code/upload_ISINs.py
+++ Code/upload_ISINs.py
@@ -7,26 +7,30 @@ REPO_NAME = "Dashboard"
 FILE_PATH = "Code/Data/ISIN-Upload/ISIN%20Input.csv"  # URL-encoded path
 
 def upload_isins_to_github(file_path, file_content):
-    github_token = os.getenv("GITHUB_TOKEN")  # Set your GitHub Personal Access Token as an environment variable
-    
+    github_token = os.getenv("GITHUB_TOKEN")
+    repo_owner = app.config["REPO_OWNER"]  # Retrieve from app.config
+    repo_name = app.config["REPO_NAME"]  # Retrieve from app.config
+    encoded_file_path = requests.utils.quote(file_path)  # URL-encode the file path
+
     if not github_token:
         return "Error: GitHub token not provided"
-    
-    url = f"{GITHUB_API_BASE_URL}/repos/{REPO_OWNER}/{REPO_NAME}/contents/{FILE_PATH}"
-    
+
+    url = f"{GITHUB_API_BASE_URL}/repos/{repo_owner}/{repo_name}/contents/{encoded_file_path}"
+
     headers = {
         "Authorization": f"Bearer {github_token}"
     }
-    
+
     response = requests.put(url, json={
         "message": "Update ISIN file",
         "content": file_content
     }, headers=headers)
-    
+
     if response.status_code == 200:
         return "File updated on GitHub successfully"
     else:
         return f"Error updating file on GitHub: {response.text}"
+
 
 # Call this function with the file content to update the file on GitHub
 def upload_isins_from_file(file_path, content_string):
